a2ba0c6288db97b0a8956ba42c0cd28ec9b91cea
Pratik Savla <pratiksavla07@gmail.com>
2020-01-28 13:10:41 +0530

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -3,95 +3,134 @@ gram = {"A":["Aa","Ab","c","d"]
 }
 #example for indirect left recursion
 gram = {
-	"A":["BC"],
-	"B":["CA","b"],
-	"C":["AA","a"]
+    "A":["BC"],
+    "B":["CA","b"],
+    "C":["AA","a"]
 
 }
 
 def removeDirectLR(gramA, A):
-	"""gramA is dictonary"""
-	temp = gramA[A]
-	tempCr = []
-	tempInCr = []
-	for i in temp:
-		if i[0] == A:
-			#tempInCr.append(i[1:])
-			tempInCr.append(i[1:]+[A+"'"])
-		else:
-			#tempCr.append(i)
-			tempCr.append(i+[A+"'"])
-	tempInCr.append(["e"])
-	gramA[A] = tempCr
-	gramA[A+"'"] = tempInCr
-	return gramA
+    """gramA is dictonary"""
+    temp = gramA[A]
+    tempCr = []
+    tempInCr = []
+    for i in temp:
+        if i[0] == A:
+            #tempInCr.append(i[1:])
+            tempInCr.append(i[1:]+[A+"'"])
+        else:
+            #tempCr.append(i)
+            tempCr.append(i+[A+"'"])
+    tempInCr.append(["e"])
+    gramA[A] = tempCr
+    gramA[A+"'"] = tempInCr
+    return gramA
 
 
 def checkForIndirect(gramA, a, ai):
-	if ai not in gramA:
-		return False 
-	if a == ai:
-		return True
-	for i in gramA[ai]:
-		if i[0] == ai:
-			return False
-		if i[0] in gramA:
-			return checkForIndirect(gramA, a, i[0])
-	return False
+    if ai not in gramA:
+        return False 
+    if a == ai:
+        return True
+    for i in gramA[ai]:
+        if i[0] == ai:
+            return False
+        if i[0] in gramA:
+            return checkForIndirect(gramA, a, i[0])
+    return False
 
 def rep(gramA, A):
-	temp = gramA[A]
-	newTemp = []
-	for i in temp:
-		if checkForIndirect(gramA, A, i[0]):
-			t = []
-			for k in gramA[i[0]]:
-				t=[]
-				t+=k
-				t+=i[1:]
-				newTemp.append(t)
+    temp = gramA[A]
+    newTemp = []
+    for i in temp:
+        if checkForIndirect(gramA, A, i[0]):
+            t = []
+            for k in gramA[i[0]]:
+                t=[]
+                t+=k
+                t+=i[1:]
+                newTemp.append(t)
 
-		else:
-			newTemp.append(i)
-	gramA[A] = newTemp
-	return gramA
+        else:
+            newTemp.append(i)
+    gramA[A] = newTemp
+    return gramA
 
 def rem(gram):
-	c = 1
-	conv = {}
-	gramA = {}
-	for j in gram:
-		conv[j] = "A"+str(c)
-		gramA["A"+str(c)] = []
-		c+=1
+    c = 1
+    conv = {}
+    gramA = {}
+    for j in gram:
+        conv[j] = "A"+str(c)
+        gramA["A"+str(c)] = []
+        c+=1
 
-	for i in gram:
-		for j in gram[i]:
-			temp = []	
-			for k in j:
-				if k in conv:
-					temp.append(conv[k])
-				else:
-					temp.append(k)
-			gramA[conv[i]].append(temp)
+    for i in gram:
+        for j in gram[i]:
+            temp = []   
+            for k in j:
+                if k in conv:
+                    temp.append(conv[k])
+                else:
+                    temp.append(k)
+            gramA[conv[i]].append(temp)
 
 
-	print(gramA)
-	for i in range(c-1,0,-1):
-		ai = "A"+str(i)
-		for j in range(0,i):
-			aj = gramA[ai][0][0]
-			if ai!=aj :
-				if aj in gramA and checkForIndirect(gramA,ai,aj):
-					gramA = rep(gramA, ai)
+    ga = gramA
+    for i in range(c-1,0,-1):
+        ai = "A"+str(i)
+        for j in range(0,i):
+            aj = gramA[ai][0][0]
+            if ai!=aj :
+                if aj in gramA and checkForIndirect(gramA,ai,aj):
+                    gramA = rep(gramA, ai)
 
-	for i in range(1,c):
-		ai = "A"+str(i)
-		for j in gramA[ai]:
-			if ai==j[0]:
-				gramA = removeDirectLR(gramA, ai)
-				break
-	print(gramA)
-	print(conv)
+    for i in range(1,c):
+        ai = "A"+str(i)
+        for j in gramA[ai]:
+            if ai==j[0]:
+                gramA = removeDirectLR(gramA, ai)
+                break
 
-rem(gram)
+    convr = {}
+    for i in conv:
+        convr[conv[i]] = i
+
+    print("Input:")
+    for i in ga:
+        if i in convr:
+            print(convr[i], end="->")
+        else:
+            print(i, end="->")
+        li = len(ga[i])-1
+
+        for j in ga[i]:
+            for k in j:
+                if k in convr:
+                    print(convr[k], end=" ")
+                else:
+                    print(k, end=" ")
+            if li>0:
+                print(end = " | ")
+                li-=1
+        print("\n", end="")
+
+    print("Output: ")
+    for i in gramA:
+        if i in convr:
+            print(convr[i], end="->")
+        else:
+            print(i, end="->")
+        li = len(gramA[i])-1
+
+        for j in gramA[i]:
+            for k in j:
+                if k in convr:
+                    print(convr[k], end=" ")
+                else:
+                    print(k, end=" ")
+            if li>0:
+                print(end = " | ")
+                li-=1
+        print("\n", end="")
+rem(gram) 
