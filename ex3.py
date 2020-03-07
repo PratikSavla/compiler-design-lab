e4ab80baa1366ebc4c143b1cb679820192b26868
PratikSavla <pratiksavla07@gmail.com>
2020-03-07 18:53:11 +0530

    Finally finished :sparkles:

--- ex3.py
+++ ex3.py
@@ -1,4 +1,3 @@
-
 # #example for direct left recursion
 # gram = {"A":["Aa","Ab","c","d"]
 # }
@@ -115,93 +114,6 @@ def rem(gram):
 	return op
 
 result = rem(gram)
-terminals = []
-for i in result:
-	for j in result[i]:
-		for k in j:
-			if k not in result:
-				terminals+=[k]
-terminals = list(set(terminals))
-#print(terminals)
 
-def first(gram, term):
-	a = []
-	if term not in gram:
-		return [term]
-	for i in gram[term]:
-		if i[0] not in gram:
-			a.append(i[0])
-		elif i[0] in gram:
-			a += first(gram, i[0])
-	return a
-
-firsts = {}
 for i in result:
-	firsts[i] = first(result,i)
-#	print(f'First({i}):',firsts[i])
-
-def follow(gram, term):
-	a = []
-	for rule in gram:
-		for i in gram[rule]:
-			if term in i:
-				temp = i
-				indx = i.index(term)
-				if indx+1!=len(i):
-					if i[-1] in firsts:
-						a+=firsts[i[-1]]
-					else:
-						a+=[i[-1]]
-				else:
-					a+=["e"]
-				if rule != term and "e" in a:
-					a+= follow(gram,rule)
-	return a
-
-follows = {}
-for i in result:
-	follows[i] = list(set(follow(result,i)))
-	if "e" in follows[i]:
-		follows[i].pop(follows[i].index("e"))
-	follows[i]+=["$"]
-#	print(f'Follow({i}):',follows[i])
-
-resMod = {}
-for i in result:
-	l = []
-	for j in result[i]:
-		temp = ""
-		for k in j:
-			temp+=k
-		l.append(temp)
-	resMod[i] = l
-
-# create predictive parsing table
-tterm = list(terminals)
-tterm.pop(tterm.index("e"))
-tterm+=["$"]
-pptable = {}
-for i in result:
-	for j in tterm:
-		if j in firsts[i]:
-			pptable[(i,j)]=resMod[i[0]][0]
-		else:
-			pptable[(i,j)]=""
-	if "e" in firsts[i]:
-		for j in tterm:
-			if j in follows[i]:
-				pptable[(i,j)]="e" 	
-pptable[("F","i")] = "i"
-toprint = f'{"": <10}'
-for i in tterm:
-	toprint+= f'|{i: <10}'
-print(toprint)
-for i in result:
-	toprint = f'{i: <10}'
-	for j in tterm:
-		if pptable[(i,j)]!="":
-			toprint+=f'|{i+"->"+pptable[(i,j)]: <10}'
-		else:
-			toprint+=f'|{pptable[(i,j)]: <10}'
-	print(f'{"-":-<76}')
-	print(toprint)
+    print(f'{i}->{result[i]}')
